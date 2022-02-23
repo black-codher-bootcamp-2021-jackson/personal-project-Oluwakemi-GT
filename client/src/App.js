d6f5d5bf247ac2aa3c50ff2e7802f37909dbc8a5
Oluwakemi-GT <oluwakemi.o@hotmail.co.uk>
2022-02-23 00:19:51 +0000

    adding login form and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,91 @@
 import React, { useState, useEffect } from "react";
+import LoginForm from "./components/LoginForm";
+
+import LoginFormStyle from "././styles/LoginFormStyle.css"
+
 
 // SERVICES THAT CALL OUR API ENDPOINTS
 import { getAllProfiles } from "./services/profileService";
 
+// function App() {
+//   const [profiles, setProfiles] = useState(null);
+
+//   useEffect(() => {
+//     async function getProfiles() {
+//       if (!profiles) {
+//         const response = await getAllProfiles();
+//         setProfiles(response);
+//       }
+//     }
+
+//     getProfiles();
+//   }, [profiles]);
+
+//   const renderProfile = (user) => {
+//     return (
+//       <li key={user._id}>
+//         <h3>
+//           {`${user.first_name} 
+//           ${user.last_name}`}
+//         </h3>
+//         <p>{user.location}</p>
+//       </li>
+//     );
+//   };
+
+//   return (
+//     <div>
+//       <ul>
+//         {profiles && profiles.length > 0 ? (
+//           profiles.map((profile) => renderProfile(profile))
+//         ) : (
+//           <p>No profiles found</p>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// }
+
 function App() {
-  const [profiles, setProfiles] = useState(null);
+  const adminUser = {
+    email: "admin@user.com",
+    password: "admin123"
+  }
 
-  useEffect(() => {
-    async function getProfiles() {
-      if (!profiles) {
-        const response = await getAllProfiles();
-        setProfiles(response);
-      }
+  const [user, setUser] = useState({ name: "", email: "" }); //default user setting
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Incorrect, try something else.");
+      setError("Incorrect, try something else.");
     }
+  }
 
-    getProfiles();
-  }, [profiles]);
-
-  const renderProfile = (user) => {
-    return (
-      <li key={user._id}>
-        <h3>
-          {`${user.first_name} 
-          ${user.last_name}`}
-        </h3>
-        <p>{user.location}</p>
-      </li>
-    );
-  };
+  const Logout = () => {
+    setUser({ name: "", email: "" }); //Logout returns to default user setting (empty)
+  }
 
   return (
-    <div>
-      <ul>
-        {profiles && profiles.length > 0 ? (
-          profiles.map((profile) => renderProfile(profile))
-        ) : (
-          <p>No profiles found</p>
-        )}
-      </ul>
+    <div className="App">
+      {(user.email != "") ? (         //if the user email is NOT showing nothing, display welcome message
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+          <LoginForm Login={Login} error={error}/>
+      )}
+     
     </div>
   );
 }
-
 export default App;
